66bb1e58e06926e10bda93ad6c0e84303840be61
aurelien biais <aurelien.biais@recaresolutions.com>
2023-02-25 14:08:42 +0100

    added main structure and api

---
+++ braintumorclassification/interface/main.py
@@ -0,0 +1,36 @@
+def preprocess():
+    """
+    Preprocess the dataset
+    """
+    return None
+
+
+def train():
+    """
+    Train a new model
+    Save final model once it has seen all data
+    Compute validation metrics
+    """
+    return None
+
+
+def evaluate():
+    """
+    Evaluate the performance of the latest production model on new data
+    """
+    return None
+
+
+def pred():
+    """
+    Make a prediction using the latest trained model
+    """
+    return None
+
+
+if __name__ == "__main__":
+    preprocess()
+    #    preprocess(source_type='val')
+    train()
+    pred()
+    evaluate()
