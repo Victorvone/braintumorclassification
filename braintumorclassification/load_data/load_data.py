d65264a98489e70e50509c056daaf7a301b15393
aurelien biais <aurelien.biais@recaresolutions.com>
2023-02-25 21:53:53 +0100

    refactored load_data to include gcloud

--- braintumorclassification/load_data/load_data.py
+++ braintumorclassification/load_data/load_data.py
@@ -1,69 +1,95 @@
 import os
 import numpy as np
-import pandas as pd
-from matplotlib.pyplot import imread
-import matplotlib.pyplot as plt
 import cv2
+from google.cloud import storage
 
-def load_data(Directory):
-    '''Returns: X_train, y_train, X_test, y_test. Loads data from data directory into np.arrays. Make sure, your directory is structured as the downloaded folder.
-    Provide the relative path to this folder as Directory argument.'''
 
-    X_train, y_train = [], []
-    X_test, y_test = [], []
+def load_data_gcloud(set):
+    """
+    loads data from the project's google cloud bucket
+    """
 
-    ### load training data
-    directories_training = {}
+    X, y = [], []
     target = 0
+    folders = ["no_tumor", "meningioma_tumor", "glioma_tumor", "pituitary_tumor"]
 
-    for foldername in os.listdir(f'{Directory}/Training'):
-        directories_training[foldername] = f'{Directory}/Training/{foldername}'
+    # Instantiates a client
+    storage_client = storage.Client()
 
-    for tumor_directory in directories_training.values():
-        for filename in os.listdir(tumor_directory):
-            filepath = os.path.join(tumor_directory, filename)
-            picture = cv2.imread(filepath)[:,:,:1]
-            picture_resized = cv2.resize(picture, (255,255))
-            X_train.append(picture_resized)
-            y_train.append(target)
+    # Get GCS bucket
+    bucket = storage_client.get_bucket(os.environ.get("BUCKET"))
+
+    for folder in folders:
+        for image in list(bucket.list_blobs(prefix=f"{set}/{folder}")):
+            picture = cv2.imdecode(
+                np.frombuffer(image.download_as_bytes(), np.uint8), -1
+            )
+            picture_resized = cv2.resize(picture, (255, 255))
+            X.append(picture_resized)
+            y.append(target)
         target += 1
 
-    ### shuffle data
-    c = list(zip(X_train, y_train))
-    np.random.shuffle(c)
-    X_train, y_train = zip(*c)
+    return X, y
 
-    ### format data to array
-    X_train = np.array(X_train)
-    X_train = np.expand_dims(X_train, axis=3)
-    y_train = np.array(y_train)
-    print(f'X_train and y_train created successfully with shapes: \n X_train:{X_train.shape}\n y_train:{y_train.shape}')
 
-    ### load testing data
-    directories_testing = {}
+def load_data_local(directory, set):
+    """
+    loads data from the local directory
+    """
+
+    X, y = [], []
+    directories = {}
     target = 0
 
-    for foldername in os.listdir(f'{Directory}/Testing'):
-        directories_testing[foldername] = f'{Directory}/Testing/{foldername}'
+    for foldername in os.listdir(f"{directory}/{set}"):
+        if not foldername.startswith("."):
+            directories[foldername] = f"{directory}/{set}/{foldername}"
 
-    for tumor_directory in directories_testing.values():
+    for tumor_directory in directories.values():
         for filename in os.listdir(tumor_directory):
             filepath = os.path.join(tumor_directory, filename)
-            picture = cv2.imread(filepath)[:,:,:1]
-            picture_resized = cv2.resize(picture, (255,255))
-            X_test.append(picture_resized)
-            y_test.append(target)
+            picture = cv2.imread(filepath)[:, :, :1]
+            picture_resized = cv2.resize(picture, (255, 255))
+            X.append(picture_resized)
+            y.append(target)
         target += 1
 
+    return X, y
+
+
+def shuffle_and_format(X, y):
     ### shuffle data
-    c = list(zip(X_test, y_test))
+    c = list(zip(X, y))
     np.random.shuffle(c)
-    X_test, y_test = zip(*c)
+    X, y = zip(*c)
 
     ### format data to array
-    X_test = np.array(X_test)
-    X_test = np.expand_dims(X_test, axis=3)
-    y_test = np.array(y_test)
-    print(f'X_test and y_test created successfully with shapes: \n X_test:{X_test.shape}\n y_train:{y_test.shape}')
+    X = np.array(X)
+    X = np.expand_dims(X, axis=3)
+    y = np.array(y)
+
+    return X, y
+
+
+def make_data_sets(directory):
+    source = os.environ.get("DATA_SOURCE")
+
+    if source == "gcloud":
+        X_train, y_train = load_data_gcloud("Training")
+        X_test, y_test = load_data_gcloud("Testing")
+
+    else:
+        X_train, y_train = load_data_local(directory, "Training")
+        X_test, y_test = load_data_local(directory, "Testing")
+
+    X_train, y_train = shuffle_and_format(X_train, y_train)
+    X_test, y_test = shuffle_and_format(X_test, y_test)
+
+    print(
+        f"X_train and y_train created successfully from {source} with shapes: \n X_train:{X_train.shape}\n y_train:{y_train.shape}"
+    )
+    print(
+        f"X_test and y_test created successfully from {source} with shapes: \n X_test:{X_test.shape}\n y_test:{y_test.shape}"
+    )
 
     return X_train, y_train, X_test, y_test
