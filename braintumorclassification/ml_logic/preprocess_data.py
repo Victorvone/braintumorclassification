a4a3b1c5589640b56aec13046f203354078d6e2f
Victor von Eisenhart-Rothe <victorvoneisenhart@gmail.com>
2023-02-28 19:28:11 +0100

    test linter

---
+++ braintumorclassification/ml_logic/preprocess_data.py
@@ -0,0 +1,15 @@
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+
+def preprocess(X_train, y_train, X_test, y_test):
+
+    #Normalizing
+    X_train_preproc = X_train/255
+    X_test_preproc = X_test/255
+
+    #OneHotEncoding
+    y_train_preproc = to_categorical(y_train, 4)
+    y_test_preproc = to_categorical(y_test, 4)
+
+    return X_train_preproc, y_train_preproc, X_test_preproc, y_test_preproc
