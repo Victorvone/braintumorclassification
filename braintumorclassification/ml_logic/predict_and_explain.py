ae5f0a25ae03ce2244ab18fc1ab924080a31e23e
Victor von Eisenhart-Rothe <victorvoneisenhart@gmail.com>
2023-03-07 21:57:03 +0100

    explainability but model not working

--- braintumorclassification/ml_logic/predict_and_explain.py
+++ braintumorclassification/ml_logic/predict_and_explain.py
@@ -3,27 +3,35 @@ from tf_explain.core.occlusion_sensitivity import OcclusionSensitivity
 from tf_explain.core.vanilla_gradients import VanillaGradients
 
 
-def predict_and_explain(model, image_array):
+def predict_and_explain(model,
+                        image_tuple,
+                        grad_cam: True,
+                        occlusion_sensitivity: True,
+                        vanilla_gradient: True):
     '''Returns prediction of tumor classification and saves Grad-CAM, Occlusion sensitivity
-    and Vanilla gradient pictures in ../visualizations folder'''
+    and Vanilla gradient pictures in ../visualizations folder. Image tuple needs
+    to contain two arrays (X, y) of the following shape: none, 255, 255, 3 and none, 4'''
 
     # prediction
-    prediction = model.predict(image_array)
+    prediction = model.predict(image_tuple)
 
     # Grad-CAM
-    gradcam = GradCAM()
-    grid = gradcam.explain(image_array, model, class_index=0, layer_name='conv2d_7')
-    # maybe change class_index to variable +
-    gradcam.save(grid, "../Visualizations/", "grad_cam.png")
+    if grad_cam is True:
+        gradcam = GradCAM()
+        grid = gradcam.explain(image_tuple, model, class_index=0, layer_name='conv2d_7')
+        # maybe change class_index to variable +
+        gradcam.save(grid, "../Visualizations/", "grad_cam.png")
 
     # Occlusion sensitivity
-    occsens = OcclusionSensitivity()
-    grid = occsens.explain(image_array, model, class_index=0, patch_size=3)
-    occsens.save(grid, "../Visualizations/", "OcclusionSensitivity.png")
+    if occlusion_sensitivity is True:
+        occsens = OcclusionSensitivity()
+        grid = occsens.explain(image_tuple, model, class_index=0, patch_size=3)
+        occsens.save(grid, "../Visualizations/", "OcclusionSensitivity.png")
 
     # Vanilla gradient
-    Vanillagrad = VanillaGradients()
-    grid = Vanillagrad.explain(image_array, model, class_index=0)
-    Vanillagrad.save(grid, "../Visualizations/", "VanillaGradient.png")
+    if vanilla_gradient is True:
+        Vanillagrad = VanillaGradients()
+        grid = Vanillagrad.explain(image_tuple, model, class_index=0)
+        Vanillagrad.save(grid, "../Visualizations/", "VanillaGradient.png")
 
     return prediction
