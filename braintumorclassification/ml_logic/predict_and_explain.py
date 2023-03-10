79f4746a443075404353140bde47d2b201bcc1f5
Victor von Eisenhart-Rothe <victorvoneisenhart@gmail.com>
2023-03-10 11:58:25 +0100

    cleaned_up

--- braintumorclassification/ml_logic/predict_and_explain.py
+++ braintumorclassification/ml_logic/predict_and_explain.py
@@ -1,37 +1,57 @@
 from tf_explain.core.grad_cam import GradCAM
 from tf_explain.core.occlusion_sensitivity import OcclusionSensitivity
 from tf_explain.core.vanilla_gradients import VanillaGradients
+import numpy as np
+from tf_explain.core.activations import ExtractActivations
 
 
 def predict_and_explain(model,
                         image_tuple,
-                        grad_cam: True,
-                        occlusion_sensitivity: True,
-                        vanilla_gradient: True):
-    '''Returns prediction of tumor classification and saves Grad-CAM, Occlusion sensitivity
-    and Vanilla gradient pictures in ../visualizations folder. Image tuple needs
+                        layers_name=None,
+                        act_viz=True,
+                        grad_cam=True,
+                        occlusion_sensitivity=True,
+                        vanilla_gradient=True):
+
+    '''Returns prediction of tumor classification, Activation visualizations, Grad-CAM, Occlusion sensitivity
+    and Vanilla gradient. It also saves pictures in ../visualizations folder. Image tuple needs
     to contain two arrays (X, y) of the following shape: none, 255, 255, 3 and none, 4'''
 
-    # prediction
-    prediction = model.predict(image_tuple)
+    # Prediction
+    prediction = model.predict(image_tuple[0])
+    class_index = np.argmax(prediction)
+
+    # Activation visualizations
+    if act_viz is True and layers_name is not None:
+        actviz = ExtractActivations()
+        grid_actviz = actviz.explain(image_tuple, model, layers_name=layers_name)
+        actviz.save(grid_actviz, "../Visualizations/", "ActViz.png")
+    else:
+        grid_actviz = None
 
     # Grad-CAM
     if grad_cam is True:
         gradcam = GradCAM()
-        grid = gradcam.explain(image_tuple, model, class_index=0, layer_name='conv2d_7')
+        grid_gradcam = gradcam.explain(image_tuple, model, class_index=class_index)
         # maybe change class_index to variable +
-        gradcam.save(grid, "../Visualizations/", "grad_cam.png")
+        gradcam.save(grid_gradcam, "../Visualizations/", "GradCam.png")
+    else:
+        grid_gradcam = None
 
     # Occlusion sensitivity
     if occlusion_sensitivity is True:
         occsens = OcclusionSensitivity()
-        grid = occsens.explain(image_tuple, model, class_index=0, patch_size=3)
-        occsens.save(grid, "../Visualizations/", "OcclusionSensitivity.png")
+        grid_occsens = occsens.explain(image_tuple, model, class_index=class_index, patch_size=8)
+        occsens.save(grid_occsens, "../Visualizations/", "OcclusionSensitivity.png")
+    else:
+        grid_occsens = None
 
     # Vanilla gradient
     if vanilla_gradient is True:
         Vanillagrad = VanillaGradients()
-        grid = Vanillagrad.explain(image_tuple, model, class_index=0)
-        Vanillagrad.save(grid, "../Visualizations/", "VanillaGradient.png")
+        grid_vanillagrad = Vanillagrad.explain(image_tuple, model, class_index=class_index)
+        Vanillagrad.save(grid_vanillagrad, "../Visualizations/", "VanillaGradient.png")
+    else:
+        grid_vanillagrad = None
 
-    return prediction
+    return prediction, grid_actviz, grid_gradcam, grid_occsens, grid_vanillagrad
