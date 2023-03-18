3d4190c22dce8dae1bd4f8e1bc6994590218ce05
aurelien biais <aurelien.biais@recaresolutions.com>
2023-03-18 16:46:25 +0100

    fixed package for docker

--- braintumorclassification/api/fast.py
+++ braintumorclassification/api/fast.py
@@ -1,9 +1,11 @@
 import sys
 from fastapi import FastAPI, File, UploadFile
 from braintumorclassification.ml_logic.predict_and_explain import predict_and_gradcam
-from braintumorclassification.ml_logic.registry import load_model  # waiting for the Local_registrypaty
-from prediction import read_image
-from prediction import preprocess
+from braintumorclassification.ml_logic.registry import (
+    load_model,
+)  # waiting for the Local_registrypaty
+from braintumorclassification.api.prediction import read_image
+from braintumorclassification.api.prediction import preprocess
 from uvicorn import run
 from starlette.responses import Response
 import io
@@ -16,20 +18,16 @@ sys.path.append("../")
 
 # Fill in the classes
 classes = ["glioma", "meningioma", "notumor", "pituitary"]
-# Fill in the Model
-filename = "/home/ivana/code/Victorvone/braintumorclassification/models/EfficientNetv2.h5"
-# filename = "/EfficientNetv2.h5" # Waiting for LOCAL_REGISTRY_PATH
 
 app = FastAPI()
-# app.state.model = load_model(filename, compile=False)
 app.state.model = load_model()
-app.state.model.compile(loss="categorical_crossentropy",
-                        optimizer="adam",
-                        metrics=["accuracy"])
+app.state.model.compile(
+    loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+)
 
 
 @app.post("/predict4")
-async def predict(file: UploadFile = File(...)):
+async def predict_image(file: UploadFile = File(...)):
     # Read the file uploaded by user
     image = read_image(await file.read())
     # after doing preprocessing
@@ -42,7 +40,7 @@ async def predict(file: UploadFile = File(...)):
     # getting a expl_image
 
     exp_image = res_total[1]
-    r = Image.fromarray(exp_image, 'RGB')
+    r = Image.fromarray(exp_image, "RGB")
 
     bytes_io = io.BytesIO()
     r.save(bytes_io, format="PNG")
@@ -51,23 +49,23 @@ async def predict(file: UploadFile = File(...)):
 
 
 @app.post("/predict5")
-async def predict(file: UploadFile = File(...)):
+async def predict_string(file: UploadFile = File(...)):
     # getting a string
     image = read_image(await file.read())
     # after doing preprocessing
     image = preprocess(image)
     # make prediction
 
-
     res_total = predict_and_gradcam(app.state.model, image)
-    res_string = predict_and_gradcam(app.state.model, image)[0][0]
+    res_string = res_total[0][0]
     max_value = res_string.max()
     # max_value = "{0:.0}%".format(max_value)
     max_position = res_string.argmax()
 
     tumor_type = classes[max_position].capitalize()
-    result_str = f'{tumor_type} has been detected with the probability {max_value:.5%} '
+    result_str = f"{tumor_type} has been detected with the probability {max_value:.5%} "
     return result_str
+
 
 @app.get("/")
 def root():
--- braintumorclassification/api/prediction.py
+++ braintumorclassification/api/prediction.py
@@ -2,16 +2,16 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 
-
-input_shape = (255,255)
+input_shape = (255, 255)
 
 
 def read_image(image_encoded):
     pil_image = Image.open(BytesIO(image_encoded))
     return pil_image
 
+
 def preprocess(image: Image.Image):
     image = image.resize(input_shape)
     image = np.asfarray(image)
-    image = np.expand_dims(image,0)
+    image = np.expand_dims(image, 0)
     return image
